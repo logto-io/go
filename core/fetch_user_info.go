e6695a221202b29a69d3e36335e841de92a646c9
ringtail <zhongwei.lzw@alibaba-inc.com>
2023-05-11 15:43:32 +0800

    fix bear token when get userInfo

--- core/fetch_user_info.go
+++ core/fetch_user_info.go
@@ -1,9 +1,19 @@
 package core
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func FetchUserInfo(client *http.Client, userInfoEndpoint, accessToken string) (UserInfoResponse, error) {
-	response, requestErr := client.Get(userInfoEndpoint)
+	req, err := http.NewRequest("GET", userInfoEndpoint, nil)
+
+	if err != nil {
+		return UserInfoResponse{}, err
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+	response, requestErr := client.Do(req)
 
 	if requestErr != nil {
 		return UserInfoResponse{}, requestErr
@@ -12,7 +22,7 @@ func FetchUserInfo(client *http.Client, userInfoEndpoint, accessToken string) (U
 	defer response.Body.Close()
 
 	var userInfoResponse UserInfoResponse
-	err := parseDataFromResponse(response, &userInfoResponse)
+	err = parseDataFromResponse(response, &userInfoResponse)
 
 	if err != nil {
 		return UserInfoResponse{}, err
