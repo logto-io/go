2706357848ae5356c55d63476ee7d3cf812210d4
Xiao Yijun <xiaoyijun@silverhand.io>
2025-05-30 16:53:58 +0800

    feat: enhance access token management with options for resource and organization

--- client/errors.go
+++ client/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrNotAuthenticated            = errors.New("not authenticated")
 	ErrUnacknowledgedResourceFound = errors.New("unacknowledged resource found")
 	ErrMissingScopeOrganizations   = errors.New("missing 'urn:logto:scope:organizations' scope")
+	ErrMissingOrganizationId       = errors.New("missing organization id")
 )
